bab3c333a2f7dfa337a72d42130311990580aa9e
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-02-26 15:06:46 -0300

    Page
    adicionado header e footer ao page

--- informatico/src/app/page.tsx
+++ informatico/src/app/page.tsx
@@ -1,9 +1,12 @@
-import Image from "next/image";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 
 export default function Home() {
   return (
-    <div>
-      
-    </div>
+    <main className="min-h-screen">
+      <Header />
+      <Footer />
+    </main>
   );
 }
