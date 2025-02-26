bc5b7a941de89cbe133965662bbe2e98c539a5da
Manoel Ferreira Matos <95552207+ManelFer@users.noreply.github.com>
2025-02-26 15:26:59 -0300

    hero
    finalizado o hero

---
+++ informatico/src/app/components/sections/Hero.tsx
@@ -0,0 +1,33 @@
+import Image from "next/image";
+import Foto1 from "../../../../public/foto1.jpg"
+
+export default function Hero() {
+    return(
+        <section id="home" className="pt-24 pb-12">
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+                <div className="grid md:grid-cols-2 gap-12 items-center">
+                    <div>
+                        <h1 className="text-4xl md:text-5xl font-bold text-gray-300 mb-6">
+                            Transformando negócios por meio de soluções inovadoras
+                        </h1>
+                        <p className="text-lg text-gray-600 mb-8">
+                            Nossa equipe de especialistas em tecnologia oferece soluções personalizadas para ajudar empresas a alcançar seus objetivos de negócio.
+                        </p>
+                        <button className="bg-white text-black px-8 py-3 rounded-lg hover:bg-gray-500 hover:text-white transition-colors">
+                            Entre em contato
+                        </button>
+                    </div>
+                    <div className="relative h-[400px]">
+                        <Image
+                            src={Foto1}
+                            alt="Informatico"
+                            fill
+                            className="object-cover rounded-lg"
+                            priority
+                        />
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
--- informatico/src/app/page.tsx
+++ informatico/src/app/page.tsx
@@ -1,11 +1,13 @@
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import Hero from "./components/sections/Hero";
 
 
 export default function Home() {
   return (
     <main className="min-h-screen">
       <Header />
+      <Hero />
       <Footer />
     </main>
   );
